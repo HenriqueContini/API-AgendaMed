44bad72c2527ef71cfcf1aceaabecb4613587c85
HenriqueContini <henrique.coutine@gmail.com>
2024-01-27 21:24:24 -0300

    Start

---
+++ src/main/java/com/henrique/APIAgendaMed/ApiAgendaMedApplication.java
@@ -0,0 +1,13 @@
+package com.henrique.APIAgendaMed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgendaMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAgendaMedApplication.class, args);
+	}
+
+}
